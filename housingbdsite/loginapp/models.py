bda2594edb80f2a4052f7205c4a64d91d8bfa6c4
debbairagi <freelancer.debbindu@gmail.com>
2023-09-21 22:29:22 +0600

    now picture is uploading

--- housingbdsite/loginapp/models.py
+++ housingbdsite/loginapp/models.py
@@ -2,8 +2,7 @@ from django.db import models
 # from django.contrib.auth import login, logout, authenticate
 
 from django.contrib.auth.models import User
-from django.core.files.storage import FileSystemStorage
-fs = FileSystemStorage(location="/media/profile_pics")
+
 
 
 # Create your models here.
@@ -11,7 +10,7 @@ fs = FileSystemStorage(location="/media/profile_pics")
 class UserInfo(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     facebook_id = models.URLField(blank=True)
-    profile_pic = models.ImageField(storage=fs , blank=True)
+    profile_pic = models.ImageField(upload_to='profile_pics' , blank=True)
 
 
     def __str__(self):
